1664f304e912adb0e0060ac7d2ebbd0d60e90bda
Siam <mdsiamulislamsoaib@gmail.com>
2025-10-01 15:59:13 +0600

    Enhance Bookmark and Explore screens with improved layouts and UI components

--- lib/feature/bookmark/views/bookmark_screen.dart
+++ lib/feature/bookmark/views/bookmark_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:ilm/core/theme/color/color.dart';
+
+import '../../../core/string_constent/path_constent/image_path.dart';
 
 class BookmarkScreen extends StatefulWidget {
   const BookmarkScreen({super.key});
@@ -11,10 +14,71 @@ class _BookmarkScreenState extends State<BookmarkScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Bookmark Screen',
-          style: Theme.of(context).textTheme.bodyLarge,
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: EdgeInsets.only(bottom: 16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
+                      child: Image(
+                        width: double.infinity,
+                        height: 200,
+                        image: AssetImage(ImagePath.prayerTimesBanner),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    SizedBox(height: 6),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Understanding the Quran: A Comprehensive Guide to Its Teachings',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 16,
+                            ),
+                          ),
+                          SizedBox(height: 6),
+                          Text(
+                            'Explore the profound teachings and historical context of the Quran. This article provides insights, key lessons, and practical applications for daily life.',
+                            style: TextStyle(color: Colors.grey[700], fontSize: 13),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          SizedBox(height: 12),
+                          Row(
+                            children: [
+                              Icon(Icons.edit, size: 18, color: Colors.blueAccent),
+                              SizedBox(width: 6),
+                              Text('Article', style: TextStyle(color: Colors.blueAccent, fontSize: 13, fontWeight: FontWeight.w500)),
+                              Spacer(),
+                              Icon(Icons.bookmark , size: 18, color: AppColors.primary),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
--- lib/feature/explore/views/explore_screen.dart
+++ lib/feature/explore/views/explore_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../core/theme/color/color.dart';
+
 class ExploreScreen extends StatefulWidget {
   const ExploreScreen({super.key});
 
@@ -11,10 +13,44 @@ class _ExploreScreenState extends State<ExploreScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Explore Screen',
-          style: Theme.of(context).textTheme.bodyLarge,
+      body: DefaultTabController(
+        length: 6,
+        child: Column(
+          children: [
+            Container(
+              color: Theme.of(context).scaffoldBackgroundColor,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: TabBar(
+                  isScrollable: true,
+                  indicatorColor: AppColors.primary,
+                  labelColor: AppColors.primary,
+                  unselectedLabelColor: Colors.grey,
+                  indicatorWeight: 2,
+                  tabs: [
+                    Tab(text: 'Articles'),
+                    Tab(text: 'Videos'),
+                    Tab(text: 'Audio'),
+                    Tab(text: 'Books'),
+                    Tab(text: 'Podcasts'),
+                    Tab(text: 'Typography'),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  Center(child: Text('Articles Content')),
+                  Center(child: Text('Videos Content')),
+                  Center(child: Text('Audio Content')),
+                  Center(child: Text('Books Content')),
+                  Center(child: Text('Podcasts Content')),
+                  Center(child: Text('Typography Content')),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
